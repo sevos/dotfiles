48261be4c1d393e1cc036dba42a421132c1dc7a1
Andrzej Śliwa <andrzej.sliwa@gmail.com>
2024-06-16 16:53:32 +0200

    update and fix for clipboard

--- config/common_nvim/.config/nvim/lua/config/options.lua
+++ config/common_nvim/.config/nvim/lua/config/options.lua
@@ -4,3 +4,16 @@
 --
 vim.opt.relativenumber = false
 vim.g.autoformat = false
+
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
