d1e0259a14345306e0ff326021ba6bd09f6e4fd6
Andrzej Śliwa <andrzej.sliwa@gmail.com>
2024-06-16 15:33:41 +0200

    improved aliases, updated telescope due to bug with buffers

--- config/common_nvim/.config/nvim/lua/config/keymaps.lua
+++ config/common_nvim/.config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,28 @@ vim.keymap.set("n", "<leader>-", "<Nop>")
 
 -- map <leader>_ to split window horizontally with description
 vim.keymap.set("n", "<leader>_", "<cmd>split<CR>")
+
+vim.keymap.set("n", "<leader>o", "<C-o>")
+
+-- -- use kj to exit insert mode
+-- -- I auto save with
+-- --  ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/auto-save.lua
+vim.keymap.set("i", "kj", "<ESC>", { desc = "Exit insert mode with kj" })
+
+-- use gh to move to the beginning of the line in normal mode
+-- use gl to move to the end of the line in normal mode
+vim.keymap.set("n", "gh", "^", { desc = "Go to the beginning of the line" })
+vim.keymap.set("n", "gl", "$", { desc = "go to the end of the line" })
+vim.keymap.set("v", "gh", "^", { desc = "Go to the beginning of the line in visual mode" })
+vim.keymap.set("v", "gl", "$", { desc = "Go to the end of the line in visual mode" })
+
+-- yank selected text into system clipboard
+-- Vim/Neovim has two clipboards: unnamed register (default) and system clipboard.
+-- Yanking with `y` goes to the unnamed register, accessible only within Vim.
+-- The system clipboard allows sharing data between Vim and other applications.
+-- Yanking with `"+y` copies text to both the unnamed register and system clipboard.
+-- The `"+` register represents the system clipboard.
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+
+-- yank/copy to end of line
+vim.keymap.set("n", "Y", "y$", { desc = "Yank to end of line" })
--- config/common_nvim/.config/nvim/lua/plugins/gitworktree.lua
+++ config/common_nvim/.config/nvim/lua/plugins/gitworktree.lua
@@ -1,20 +1,21 @@
-return {
-  {
-    "polarmutex/git-worktree.nvim",
-    config = function()
-      require("git-worktree").setup({
-        update_on_change = true,
-        autopush = true,
-      })
-      require("telescope").load_extension("git_worktree")
-    end,
-    keys = {
-      { "<leader>gW", [[<cmd>lua require('git-worktree').create_worktree()<CR>]], desc = "Create worktree" },
-      {
-        "<leader>gw",
-        [[<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>]],
-        desc = "Switch worktree",
-      },
-    },
-  },
-}
+return {}
+
+--   {
+--     "polarmutex/git-worktree.nvim",
+--     config = function()
+--       require("git-worktree").setup({
+--         update_on_change = true,
+--         autopush = true,
+--       })
+--       require("telescope").load_extension("git_worktree")
+--     end,
+--     keys = {
+--       { "<leader>gW", [[<cmd>lua require('git-worktree').create_worktree()<CR>]], desc = "Create worktree" },
+--       {
+--         "<leader>gw",
+--         [[<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>]],
+--         desc = "Switch worktree",
+--       },
+--     },
+--   },
+-- }
